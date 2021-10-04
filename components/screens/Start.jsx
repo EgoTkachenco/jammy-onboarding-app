f03f639cfbbafb7421fa3c99ce9ff29cae102388
EgoTkachenco <egortkachenco@gmail.com>
2021-10-04 09:14:30 +0300

    Add start screen background

--- components/screens/Start.jsx
+++ components/screens/Start.jsx
@@ -1,18 +1,12 @@
-import Image from 'next/image'
-
 export default function WelcomScreen(props) {
   return (
-    <div className="page-container no-navigation centered connect-midi-device">
-      {/* <div className="lg-text text-center">
-        Hi there, thanks for choosing Jammy!
-      </div>
-      <div className="title-text text-center">
-        {"Let's fine-tune your Jammy"} <br /> according to your preferences
-      </div>
-      <div className="lg-text text-center">
-        Turn your Jammy on and connect <br /> it to your computer via USB cable
-      </div> */}
-
+    <div className="page-container no-navigation centered start-screen">
+      <video
+        autoPlay
+        muted
+        loop
+        src="/Jammy E_ How to assemble and connect.mp4"
+      />
       <button className="btn btn-primary" onClick={props.action}>
         Start
       </button>
